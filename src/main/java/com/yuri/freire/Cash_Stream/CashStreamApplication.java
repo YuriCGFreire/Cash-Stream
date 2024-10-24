b2eb285765fa08b257d1c1fbf389da425156d065
YuriFreireG <yurichandlleigurgelfreire@gmail.com>
2024-10-24 14:40:43 -0300

    initial commi

---
+++ src/main/java/com/yuri/freire/Cash_Stream/CashStreamApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.freire.Cash_Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/freire/Cash_Stream/CashStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.freire.Cash_Stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

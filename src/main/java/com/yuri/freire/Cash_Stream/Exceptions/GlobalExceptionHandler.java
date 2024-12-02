9825083d2d55c6c7909f91cadb4e52a9cc4895c1
YuriFreireG <yurichandlleigurgelfreire@gmail.com>
2024-12-02 13:19:41 -0300

    Create exception handler

--- src/main/java/com/yuri/freire/Cash_Stream/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/yuri/freire/Cash_Stream/Exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,38 @@
-package com.yuri.freire.Cash_Stream.Exceptions;public class GlobalExceptionHandler {
+package com.yuri.freire.Cash_Stream.Exceptions;
+
+import com.yuri.freire.Cash_Stream.Response.ApiResponse;
+import com.yuri.freire.Cash_Stream.Response.ResponseUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ValidationException;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(HttpServletRequest request, Exception exception){
+        List<String> errors = Arrays.asList(exception.getMessage());
+        ApiResponse<Void> response = ResponseUtil.error(errors, "An error ocurred", 1000, request.getRequestURI());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResponseStatusException(HttpServletRequest request, ResponseStatusException responseStatusException){
+        ApiResponse<Void> response = ResponseUtil.error(responseStatusException.getMessage(), "Resource not found", 1001, request.getRequestURI());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(HttpServletRequest request, ValidationException validationException){
+        ApiResponse<Void> response  = ResponseUtil.error(validationException.getMessage(), "Validation Failed", 1002, request.getRequestURI());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }

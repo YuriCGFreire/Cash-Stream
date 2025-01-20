c3d4d249db84fdbce105c1f0512879576073f085
YuriCGFreire <yurifreireg@gmail.com>
2025-01-19 21:12:00 -0300

    Teste

--- src/main/java/com/yuri/freire/Cash_Stream/CashStreamApplication.java
+++ src/main/java/com/yuri/freire/Cash_Stream/CashStreamApplication.java
@@ -3,7 +3,7 @@ package com.yuri.freire.Cash_Stream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class CashStreamApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CashStreamApplication.class, args);
